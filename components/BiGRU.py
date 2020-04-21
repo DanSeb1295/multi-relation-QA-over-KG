ad5b9997f516128294bd54b05d0e3a12e02fa7b2
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-21 11:53:05 +0800

    fixed bigru

--- components/BiGRU.py
+++ components/BiGRU.py
@@ -7,20 +7,23 @@ dropout = 0.3
 
 class BiGRU():
     def __init__(self):
-        self.fw_1 = keras.layers.GRU(HIDDEN_DIM, dropout=dropout, recurrent_dropout=dropout)
-        self.fw_2 = keras.layers.GRU(HIDDEN_DIM, dropout=dropout, recurrent_dropout=dropout)
-        self.bw_1 = keras.layers.GRU(HIDDEN_DIM, go_backwards=True, dropout=dropout, recurrent_dropout=dropout)
-        self.bw_2 = keras.layers.GRU(HIDDEN_DIM, go_backwards=True, dropout=dropout, recurrent_dropout=dropout)
+        self.fw_1 = keras.layers.GRU(HIDDEN_DIM, dropout=dropout, recurrent_dropout=dropout, return_sequences=True)
+        self.fw_2 = keras.layers.GRU(HIDDEN_DIM, dropout=dropout, recurrent_dropout=dropout, return_sequences=True)
+        self.bw_1 = keras.layers.GRU(HIDDEN_DIM, go_backwards=True, dropout=dropout, recurrent_dropout=dropout, return_sequences=True)
+        self.bw_2 = keras.layers.GRU(HIDDEN_DIM, go_backwards=True, dropout=dropout, recurrent_dropout=dropout, return_sequences=True)
 
     def compute(self, q):
-        print(q)
-        fw_out_1 = self.fw_1(q)
-        bw_out_1 = self.bw_1(q)
+        n = q.shape[1]
+
+        fw_out_1 = fw_1(q)
+        bw_out_1 = bw_1(q)
         hidden_out = tf.concat([fw_out_1, bw_out_1], 0)
+        hidden_out = tf.reshape(hidden_out, (1, n, d))
         
-        fw_out_2 = self.fw_2(hidden_out)
-        bw_out_2 = self.bw_2(hidden_out)
+        fw_out_2 = fw_2(hidden_out)
+        bw_out_2 = bw_2(hidden_out)
         output = tf.concat([fw_out_2, bw_out_2], 0)
+        output = tf.reshape(output, (1, n, d))
 
         return output
 
