97a8e1dfc44e067b2f56bc86b33fa2f9507db8b6
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-21 16:36:16 +0800

    polnet softmax and sampling

--- PolicyNetwork.py
+++ PolicyNetwork.py
@@ -297,7 +297,7 @@ class PolicyNetwork():
         # Draw one example from the distribution (we could draw more)
         index = tf.compat.v1.multinomial(rescaled_probas, num_samples=1)
         index = tf.squeeze(index, [0])
-        index = tf.keras.backend.eval(index)[0]
+        index = K.eval(index)[0]
         return actions[index]
 
 
