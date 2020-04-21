197d00796cec3f1d56d9bdbb94ed65345a97b852
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-21 14:49:55 +0800

    fix Attention

--- components/Attention.py
+++ components/Attention.py
@@ -14,18 +14,15 @@ class Attention():
 
         r_star = tf.reshape(r_star, [d])
 
-        beta_stars = []
-        for w_t_m in q_t:
-            output = tf.multiply(r_star, w_t_m)
-            b_star = tf.matmul(self.W, output) + self.b
-            beta_stars.append(b_star)
+        beta_stars = tf.map_fn(lambda w_t_m: self.beta_stars(r_star, w_t_m), q_t)
         alpha_stars = tf.nn.softmax(beta_stars)
         
-        q_t_stars = []
-        for i, a_star in enumerate(alpha_stars):
-            q = a_star * q_t[i]
-            q_t_stars.append(q)
+        q_t_stars = tf.math.multiply(alpha_stars, q_t)
         q_t_star = tf.reduce_sum(q_t_stars, 0)
 
         return q_t_star
 
+    def beta_stars(self, r_star, w_t_m):
+        output = tf.multiply(r_star, w_t_m)
+        b_star = tf.matmul(self.W, output) + self.b
+        return b_star
