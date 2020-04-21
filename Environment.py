49e018976b880d5045f829736cdd9e878c861df9
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-21 23:59:47 +0800

    env bug

--- Environment.py
+++ Environment.py
@@ -46,10 +46,10 @@ class Rewards():
         
         if t == 1: return 0
         
-        n = len(q)
         H_t = state_memory.get('H_t')
         q_t = state_memory.get('q_t')
         Q_t = np.zeros(d)
+        n = len(q.get(t. []))
         for i in range(1, t):
             for j in range(n):
                 Q_t += q_t[i][j]
