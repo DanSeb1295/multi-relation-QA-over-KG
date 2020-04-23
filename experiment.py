ebe48eee520a959ffe7896a2f1ef2aea7b35fee1
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-24 04:11:28 +0800

    plot results

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 from PolicyNetwork import PolicyNetwork
 from Environment import Environment
 from util import prep_dataset, fetch_model_name
+from plot import plot_results
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
@@ -37,24 +38,5 @@ model_name = fetch_model_name('perceptron')
 policy_network = PolicyNetwork(T, model_name)
 train_per, val_per = policy_network.train(inputs, epochs=epochs, attention=False)		# Model does not use attention layer
 
-
-# print(pd.DataFrame({'SRN': [np.mean(val_att_per[0])], 'w/o Attention': [np.mean(val_per[0])],
-#                     'w/o Perceptron': [np.mean(val_att[0])]}, index=["PQ-3H"]).T)
-
 # Plot Results
-# xaxis = range(1, epochs + 1)
-# ymin = 0
-# ymax = 1
-# fig = plt.figure()
-
-# plt.title("Ablation Study of PQ-3H")
-# plt.xlabel('Epochs')
-# plt.ylabel('Validation Set Hits@1')
-# plt.plot(xaxis, val_att_per[0], 'r', label='SRN')
-# plt.plot(xaxis, val_per[0], 'b', label='no Attention layer')
-# plt.plot(xaxis, val_att[0], 'g', label='no Perceptron layer')
-# axes = plt.gca()
-# axes.set_ylim([ymin, ymax])
-
-# plt.legend()
-# plt.show()
+plot_results()
