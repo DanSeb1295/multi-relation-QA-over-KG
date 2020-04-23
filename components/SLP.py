674f6e0055b2ab1e69bcebd4b5611fa73ff465ad
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-04-23 16:23:49 +0800

    SLP fix

--- components/SLP.py
+++ components/SLP.py
@@ -7,17 +7,18 @@ initializer = tf.keras.initializers.GlorotNormal()
 class SLP(layers.Layer):
     def __init__(self, T):
         super(SLP, self).__init__()
-        self.weights_dict = {}
-        self.biases_dict = {}
-
         for t in range(1, T+1):
-            self.weights_dict[t] = tf.Variable(initializer([d, d]), trainable=True)
-            self.biases_dict[t] = tf.Variable(initializer([d]), trainable=True)
+            weight = 'weights_{}'.format(t)
+            bias = 'biases_{}'.format(t)
+            w = tf.Variable(initializer([d, d]), trainable=True)
+            b = tf.Variable(initializer([d]), trainable=True)
+            setattr(self, weight, w)
+            setattr(self, bias, b)
     
     def call(self, q, t):
-        w_t = self.weights_dict[t]
-        b_t = self.biases_dict[t]
-
+        w_t = getattr(self, 'weights_{}'.format(t))
+        b_t = getattr(self, 'biases_{}'.format(t))
+        
         logits = tf.matmul(q, w_t)
         logits = logits + b_t
         q_t = tf.math.tanh(logits)
